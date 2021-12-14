a58d0a904e20099d7f1cea6701d5a24270f39378
augilar <vk17varshith@gmail.com>
2021-12-14 22:53:52 +0530

    added a package.json file and gitignore file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 const fs = require("fs");
-const torrent = fs.readFileSync('puppy.torrent');//readFileSync - easiest way to read a file, but instead of returning a string it returns a buffer, so we have to convert into a string
-console.log(torrent.toString('utf8'));
+const bencode = require("bencode");
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));//readFileSync - easiest way to read a file, but instead of returning a string it returns a buffer, so we have to convert into a string
+console.log(torrent.announce.toString('utf8'));
