77e1ed82932e484a209c39397a488756cd2b1624
augilar <k.varshith@iitg.ac.in>
2021-12-23 18:39:56 +0530

    added onWholeMsg function in download.js

--- src/download.js
+++ src/download.js
@@ -16,7 +16,24 @@ function download(peer) {
     socket.connect(peer.port, peer.ip, () => {
         console.log("tcp connection made");
     });
-    socket.on('data', data => {
+    onWholeMsg(socket, data => {
 
+    });
+}
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        //len of the whole msg
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readUInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+        while(savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+            callback(savedBuf.slice(0, msgLen()));  //because part of next message may also be there
+            savedBuf = savedBuf.slice(msgLen()); //new message part
+            handshake = false;
+        }
     });
 }
